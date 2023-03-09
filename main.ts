3fcf5d71c45bbb08ae0f3559f84f1544e247fd47
1001dy2 <115143514+1001dy2@users.noreply.github.com>
2023-03-09 16:46:28 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+})
+radio.setGroup(87)
+let 出拳 = 0
